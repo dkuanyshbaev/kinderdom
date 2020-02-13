3a31ef35d4564cb1eef3f0119cd2a99df84d48b2
denis <orca84@protonmail.com>
2020-02-13 17:22:18 +1000

    WIP: cleanup at profile model

--- src/models/profile.rs
+++ src/models/profile.rs
@@ -21,9 +21,7 @@ pub struct Profiles {
 
 impl Profile {
     pub fn all(connection: &PgConnection) -> QueryResult<Vec<Profile>> {
-        profiles::table
-            .order(profiles::id.desc())
-            .load::<Profile>(connection)
+        profiles::table.order(profiles::id.desc()).load(connection)
     }
 
     pub fn published(connection: &PgConnection) -> QueryResult<Vec<Profile>> {
@@ -31,32 +29,26 @@ impl Profile {
             .filter(profiles::published.eq(true))
             .limit(4)
             .order(profiles::id.desc())
-            .load::<Profile>(connection)
+            .load(connection)
     }
 
     pub fn get(connection: &PgConnection, id: i32) -> QueryResult<Profile> {
-        profiles::table.find(id).get_result::<Profile>(connection)
+        profiles::table.find(id).get_result(connection)
     }
 
     pub fn insert(connection: &PgConnection, profile: Profile) -> QueryResult<Profile> {
         diesel::insert_into(profiles::table)
             .values(profile)
-            .get_result::<Profile>(connection)
+            .get_result(connection)
     }
 
     pub fn update(connection: &PgConnection, profile: Profile, id: i32) -> QueryResult<Profile> {
-        // let old_profile = profiles::table.find(id).get_result::<Profile>(connection);
-        //
-        // if old_profile.is_err() {
-        //     // return
-        // }
-
         diesel::update(profiles::table.find(id))
             .set(profile)
-            .get_result::<Profile>(connection)
+            .get_result(connection)
     }
 
     pub fn delete(connection: &PgConnection, id: i32) -> QueryResult<Profile> {
-        diesel::delete(profiles::table.find(id)).get_result::<Profile>(connection)
+        diesel::delete(profiles::table.find(id)).get_result(connection)
     }
 }
