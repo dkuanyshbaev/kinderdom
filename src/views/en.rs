5cad7a15a1f6c37b8bce6cc7f346b26f71ad20a3
Denis K <konsistentsi@gmail.com>
2021-06-06 17:54:47 +1000

    ADD: en index

--- src/views/en.rs
+++ src/views/en.rs
@@ -1,13 +1,81 @@
+use crate::models::cat::Cat;
 use crate::models::cause::Cause;
 use crate::models::event::Event;
-use crate::{Db, KinderResult};
+use crate::models::payment::{
+    Amount, PaymentBody, PaymentForm, PaymentResponse, RequestConfirmation,
+};
+use crate::models::profile::Profile;
+use crate::models::report::Report;
+use crate::models::search::SearchForm;
+use crate::{Config, Db, KinderResult};
+use base64::encode;
+use reqwest::header::AUTHORIZATION;
+use reqwest::StatusCode;
+use rocket::request::Form;
+use rocket::response::Redirect;
+use rocket::State;
 use rocket_contrib::templates::Template;
+use uuid::Uuid;
 
 #[derive(Serialize)]
 pub struct IndexContext {
     causes: Vec<Cause>,
     events: Vec<Event>,
     stories: Vec<Event>,
+}
+
+#[derive(Serialize)]
+pub struct EventsContext {
+    total: u8,
+    page: u8,
+    cat: u8,
+    events: Vec<Event>,
+}
+
+#[derive(Serialize)]
+pub struct EventContext {
+    cats: Vec<Cat>,
+    event: Event,
+    causes: Vec<Cause>,
+}
+
+#[derive(Serialize)]
+pub struct CausesContext {
+    causes: Vec<Cause>,
+}
+
+#[derive(Serialize)]
+pub struct CauseContext {
+    cause: Cause,
+    vitals: Vec<Cause>,
+}
+
+#[derive(Serialize)]
+pub struct ProfilesContext {
+    total: u8,
+    page: u8,
+    profiles: Vec<Profile>,
+}
+
+#[derive(Serialize)]
+pub struct ProfileContext {
+    profile: Profile,
+    vitals: Vec<Cause>,
+}
+
+#[derive(Serialize)]
+pub struct ReportsContext {
+    // total: u8,
+    // page: u8,
+    reports: Vec<Report>,
+}
+
+#[derive(Serialize)]
+pub struct NoContext {}
+
+#[derive(Serialize)]
+pub struct SearchContext {
+    events: Vec<Event>,
 }
 
 #[get("/")]
@@ -21,6 +89,117 @@ pub fn index_en(connection: Db) -> KinderResult<Template> {
             causes: vitals,
             events: last,
             stories: stories,
+        },
+    ))
+}
+
+#[get("/events?<page>&<cat>")]
+pub fn events_en(connection: Db, page: Option<u8>, cat: Option<u8>) -> KinderResult<Template> {
+    let (total, page, cat, events) = Event::paginated_by_cat(&connection, page, cat)?;
+
+    Ok(Template::render(
+        "en/events_en",
+        EventsContext {
+            total,
+            page,
+            cat,
+            events,
+        },
+    ))
+}
+
+#[get("/events/<id>")]
+pub fn event_details_en(connection: Db, id: i32) -> KinderResult<Template> {
+    Ok(Template::render(
+        "en/event_details_en",
+        EventContext {
+            cats: Cat::ru(&connection)?,
+            event: Event::get(&connection, id)?,
+            causes: Cause::vital(&connection)?,
+        },
+    ))
+}
+
+#[get("/causes")]
+pub fn causes_en(connection: Db) -> KinderResult<Template> {
+    Ok(Template::render(
+        "en/causes_en",
+        CausesContext {
+            causes: Cause::published(&connection)?,
+        },
+    ))
+}
+
+#[get("/causes/<id>")]
+pub fn cause_details_en(connection: Db, id: i32) -> KinderResult<Template> {
+    Ok(Template::render(
+        "en/cause_details_en",
+        CauseContext {
+            cause: Cause::get(&connection, id)?,
+            vitals: Cause::vital(&connection)?,
+        },
+    ))
+}
+
+#[get("/profiles?<page>")]
+pub fn profiles_en(connection: Db, page: Option<u8>) -> KinderResult<Template> {
+    let (total, page, profiles) = Profile::paginated(&connection, page)?;
+
+    Ok(Template::render(
+        "en/profiles_en",
+        ProfilesContext {
+            total,
+            page,
+            profiles,
+        },
+    ))
+}
+
+#[get("/profiles/<id>")]
+pub fn profile_details_en(connection: Db, id: i32) -> KinderResult<Template> {
+    Ok(Template::render(
+        "en/profile_details_en",
+        ProfileContext {
+            profile: Profile::get(&connection, id)?,
+            vitals: Cause::vital(&connection)?,
+        },
+    ))
+}
+
+// #[get("/reports?<page>")]
+// pub fn reports(connection: Db, page: Option<u8>) -> KinderResult<Template> {
+#[get("/reports")]
+pub fn reports_en(connection: Db) -> KinderResult<Template> {
+    Ok(Template::render(
+        "en/reports_en",
+        ReportsContext {
+            reports: Report::all(&connection)?,
+        },
+    ))
+
+    // let (total, page, reports) = Report::paginated(&connection, page)?;
+    //
+    // Ok(Template::render(
+    //     "pages/reports",
+    //     ReportsContext {
+    //         total,
+    //         page,
+    //         reports,
+    //     },
+    // ))
+}
+
+#[get("/about")]
+pub fn about_en() -> Template {
+    Template::render("en/about_en", NoContext {})
+}
+
+#[post("/search", data = "<search_form>")]
+pub fn search_en(connection: Db, search_form: Form<SearchForm>) -> KinderResult<Template> {
+    Ok(Template::render(
+        "en/results_en",
+        SearchContext {
+            events: Event::search(&connection, search_form.term.to_owned())?,
         },
     ))
 }
